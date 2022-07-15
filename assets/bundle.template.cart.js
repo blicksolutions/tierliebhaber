c44ac253e8cd02cb44e58eea796aa67099731aa0
marcel <marcel@blicksolutions.de>
2022-07-15 11:11:45 +0200

    link atc bundle to cart

--- assets/bundle.template.cart.js
+++ assets/bundle.template.cart.js
@@ -13,6 +13,7 @@ window.addEventListener('load', function () {
                 console.log(discountCodeName)
 
                 if (discountCodeName.scrollWidth < 2) {
+                    console.log("empty code SO RELOAD")
                     window.location.reload();
                 }
 
