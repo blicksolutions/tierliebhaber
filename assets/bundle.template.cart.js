7e2e127eedfbeae060b763e9f756dd889e5003ad
marcel <marcel@blicksolutions.de>
2022-07-15 10:20:26 +0200

    link atc bundle to cart

--- assets/bundle.template.cart.js
+++ assets/bundle.template.cart.js
@@ -1,18 +1,13 @@
-console.log("CARTTTTTTT")
 window.addEventListener('load', function () {
     const interval = setInterval(() => {
         const cartFooter = document.querySelector('.Cart__Footer');
         if (cartFooter) {
-            console.log("CARTFOOTER IS THERE")
-
             const discountCodeName = cartFooter.querySelector('.code-name');
-            console.log("INTERVALLLLL")
-
-            console.log(discountCodeName)
 
             if (discountCodeName) {
-                console.log("DISCOUNTCODE IS THERE")
                 clearInterval(interval);
+                console.log("IMMA RELOAD CYA")
+                window.location.reload();
             }
         }
     }, 500);
