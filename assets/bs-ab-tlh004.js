59317e7ce38659134b156b1f4bb6064aa8a11a47
Sanaxo <manuel@blicksolutions.de>
2024-04-02 15:35:31 +0200

    add logo

--- assets/bs-ab-tlh004.js
+++ assets/bs-ab-tlh004.js
@@ -1,5 +1,13 @@
 window.activateAbTlh004 = () => {
+    const shippingLogoMarkup = () => {
+        return `
+            <img class="shipping__logo" src="https://cdn.shopify.com/s/files/1/0587/2746/5119/files/dhl-go-green-logo.png?v=1712064022">
+        `;
+    }
+
     if (window.Shopify.Checkout.step == 'shipping_method') {
+
+
 
         const shippingMethodListTitle = document.querySelector('.section--shipping-method #main-header');
         if (!shippingMethodListTitle) return;
@@ -9,6 +17,8 @@ window.activateAbTlh004 = () => {
         shippingMethodListTitle.innerText = 'DHL Blitzversand';
 
         shippingMethodLabel.innerHTML = shippingMethodLabel.innerHTML.replace('Standard','Versicherter Premium Versand + DHL green Logo');
+        
+        shippingMethodLabel.insertAdjacentHTML('afterbegin', shippingLogoMarkup());
 
     } else return;
 }
