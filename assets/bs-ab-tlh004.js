3923e8b7ce5dc35bdcd810afd67cea35354eb6d2
Sanaxo <manuel@blicksolutions.de>
2024-04-02 14:14:18 +0200

    Update bs-ab-tlh004.js

--- assets/bs-ab-tlh004.js
+++ assets/bs-ab-tlh004.js
@@ -6,7 +6,6 @@ window.activateAbTlh004 = () => {
         const shippingMethodLabel = document.querySelector('.section--shipping-method [data-shipping-methods] [data-shipping-method-label-title]')
         if (!shippingMethodLabel) return
 
-
         shippingMethodListTitle.innerText = 'DHL Blitzversand'
 
         shippingMethodLabel.innerHTML = shippingMethodLabel.innerHTML.replace('Standard','Versicherter Premium Versand + DHL green Logo')
