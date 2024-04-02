af3a90da585097380f4c4e25912c7f88d7c92777
Sanaxo <manuel@blicksolutions.de>
2024-04-02 13:12:11 +0200

    add ab test

---
+++ assets/bs-ab-tlh004.js
@@ -0,0 +1,16 @@
+window.activateAbTlh004 = () => {
+    if (window.Shopify.Checkout.step == 'shipping_method') {
+
+        const shippingMethodListTitle = document.querySelector('.section--shipping-method #main-header')
+        if (!shippingMethodListTitle) return
+        const shippingMethodLabel = document.querySelector('.section--shipping-method [data-shipping-methods] [data-shipping-method-label-title]')
+        if (!shippingMethodLabel) return
+
+
+        shippingMethodListTitle.innerText = 'Versicherter Premium Versand + DHL green Logo'
+
+        shippingMethodLabel.innerHTML = shippingMethodLabel.innerHTML.replace('Standard','Versicherter Premium Versand + DHL green Logo')
+
+    } else return
+
+}
