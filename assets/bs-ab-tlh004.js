c6f61ef4a0d9089edcd15126a40b2f825a840f1f
Sanaxo <manuel@blicksolutions.de>
2024-04-02 14:16:45 +0200

    Update bs-ab-tlh004.js

--- assets/bs-ab-tlh004.js
+++ assets/bs-ab-tlh004.js
@@ -1,14 +1,14 @@
 window.activateAbTlh004 = () => {
     if (window.Shopify.Checkout.step == 'shipping_method') {
 
-        const shippingMethodListTitle = document.querySelector('.section--shipping-method #main-header')
-        if (!shippingMethodListTitle) return
-        const shippingMethodLabel = document.querySelector('.section--shipping-method [data-shipping-methods] [data-shipping-method-label-title]')
-        if (!shippingMethodLabel) return
+        const shippingMethodListTitle = document.querySelector('.section--shipping-method #main-header');
+        if (!shippingMethodListTitle) return;
+        const shippingMethodLabel = document.querySelector('.section--shipping-method [data-shipping-methods] [data-shipping-method-label-title]');
+        if (!shippingMethodLabel) return;
 
-        shippingMethodListTitle.innerText = 'DHL Blitzversand'
+        shippingMethodListTitle.innerText = 'DHL Blitzversand';
 
-        shippingMethodLabel.innerHTML = shippingMethodLabel.innerHTML.replace('Standard','Versicherter Premium Versand + DHL green Logo')
+        shippingMethodLabel.innerHTML = shippingMethodLabel.innerHTML.replace('Standard','Versicherter Premium Versand + DHL green Logo');
 
-    } else return
+    } else return;
 }
