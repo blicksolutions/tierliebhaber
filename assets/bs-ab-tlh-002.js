4b52069d8289b12808cee76b244d431a3011cbf3
Sanaxo <manuel@blicksolutions.de>
2024-04-03 10:42:21 +0200

    Update bs-ab-tlh-002.js

--- assets/bs-ab-tlh-002.js
+++ assets/bs-ab-tlh-002.js
@@ -13,23 +13,27 @@ window.activateAbTlh002 = () => {
         if (atcButtonPosition.top < 0) {
             stickyATC.classList.add('active');
 
-            // Hiding other app overlays
-            const superchatWidget = document.querySelector('#superchat-widget')
-            const smileUiContainer = document.querySelector('#smile-ui-lite-container')
-
-            superchatWidget.style.display = 'none'
-            smileUiContainer.style.display = 'none'
+            if (window.innerWidth <= 768) {
+                // Hiding other app overlays
+                const superchatWidget = document.querySelector('#superchat-widget')
+                const smileUiContainer = document.querySelector('#smile-ui-lite-container')
+    
+                superchatWidget.style.display = 'none'
+                smileUiContainer.style.display = 'none'
+            }
         }
 
         if (atcButtonPosition.top > 0) {
             stickyATC.classList.remove('active');
 
-            // Showing other app overlays
-            const superchatWidget = document.querySelector('#superchat-widget')
-            const smileUiContainer = document.querySelector('#smile-ui-lite-container')
-
-            superchatWidget.style.display = 'block'
-            smileUiContainer.style.display = 'block'
+            if (window.innerWidth <= 768) {
+                // Showing other app overlays
+                const superchatWidget = document.querySelector('#superchat-widget')
+                const smileUiContainer = document.querySelector('#smile-ui-lite-container')
+    
+                superchatWidget.style.display = 'block'
+                smileUiContainer.style.display = 'block'
+            }
         }
     });
 
