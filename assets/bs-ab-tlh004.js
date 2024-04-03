255f0a6861d5e011a754a0d448c7181b78888adb
Sanaxo <manuel@blicksolutions.de>
2024-04-03 09:32:05 +0200

    Update bs-ab-tlh004.js

--- assets/bs-ab-tlh004.js
+++ assets/bs-ab-tlh004.js
@@ -3,15 +3,13 @@ window.activateAbTlh004 = () => {
         return `
             <img class="shipping__logo" src="https://cdn.shopify.com/s/files/1/0587/2746/5119/files/dhl-go-green-logo.png?v=1712064022">
         `;
-    }
+    };
 
     if (window.Shopify.Checkout.step == 'shipping_method') {
-
-
-
         const shippingMethodListTitle = document.querySelector('.section--shipping-method #main-header');
         if (!shippingMethodListTitle) return;
-        const shippingMethodContainer = document.querySelector('[data-shipping-methods]')
+        
+        const shippingMethodContainer = document.querySelector('[data-shipping-methods]');
         if (!shippingMethodContainer) return;
 
         const shippingMethodLabel = document.querySelector('.section--shipping-method [data-shipping-methods] [data-shipping-method-label-title]');
@@ -24,4 +22,4 @@ window.activateAbTlh004 = () => {
         shippingMethodContainer.insertAdjacentHTML('beforeend', shippingLogoMarkup());
 
     } else return;
-}
+};
