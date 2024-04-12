0e9318c9d440c039224e40b25e459ecd915b6f9c
marcel <marcel@blicksolutions.de>
2024-04-12 12:43:56 +0200

    AB/TLH-007

---
+++ assets/bs-ab-tlh-007.js
@@ -0,0 +1,8 @@
+window.activateAbTlh007 = () => {
+    const cartDrawer = document.querySelector('#sidebar-cart');
+    cartDrawer.classList.add('Drawer--b');
+};
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     window.activateAbTlh007();
+// });
