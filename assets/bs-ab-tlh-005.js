8b906145d5e0b85f5e230affb7a626da42a8eafd
marcel <marcel@blicksolutions.de>
2024-04-12 13:43:19 +0200

    AB/TLH-005

---
+++ assets/bs-ab-tlh-005.js
@@ -0,0 +1,8 @@
+window.activateAbTlh005 = () => {
+    window.progressBarMin = 15;
+    window.progressBarMax = 26;
+};
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     window.activateAbTlh005();
+// });
