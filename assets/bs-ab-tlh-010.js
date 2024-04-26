5ee9a52ce90b4a3fbdd7d57c42eda695006e04e3
Sanaxo <manuel@blicksolutions.de>
2024-04-26 13:29:50 +0200

    add tlh-010

---
+++ assets/bs-ab-tlh-010.js
@@ -0,0 +1,8 @@
+window.activateAbTlh010 = () => {
+    const megamenuElement = document.querySelector('.Header__MainNav');
+
+    megamenuElement.setAttribute('data-js-tlh-010', true)
+
+};
+
+document.addEventListener('DOMContentLoaded', window.activateAbTlh010)
