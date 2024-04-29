510200884b660daf11608714a34ef4954dd2733c
Sanaxo <manuel@blicksolutions.de>
2024-04-29 10:16:46 +0200

    Update bs-ab-tlh-010.js

--- assets/bs-ab-tlh-010.js
+++ assets/bs-ab-tlh-010.js
@@ -1,8 +1,5 @@
 window.activateAbTlh010 = () => {
     const megamenuElement = document.querySelector('.Header__MainNav');
 
-    megamenuElement.setAttribute('data-js-tlh-010', true)
-
+    megamenuElement.setAttribute('data-js-tlh-010', true);
 };
-
-document.addEventListener('DOMContentLoaded', window.activateAbTlh010)
