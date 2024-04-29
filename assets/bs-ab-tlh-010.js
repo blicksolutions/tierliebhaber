99831c18b3c49b1c95850155aae58809e4993155
Sanaxo <manuel@blicksolutions.de>
2024-04-29 10:40:26 +0200

    Update bs-ab-tlh-010.js

--- assets/bs-ab-tlh-010.js
+++ assets/bs-ab-tlh-010.js
@@ -3,4 +3,14 @@ window.activateAbTlh010 = () => {
     if (!megamenuElement) return;
 
     megamenuElement.setAttribute('data-js-tlh-010', true);
+
+    const targetElements = megamenuElement.querySelectorAll('.ProductItem, .collection-dropdown__link');
+
+    targetElements.forEach((targetElement) => {
+        targetElement.addEventListener('click',() => {
+            gtag('event', 'bs_megamenu_collection_clickrate', {
+                'bs_megamenu_collection_clickrate': '1'
+            });
+        });
+    });
 };
