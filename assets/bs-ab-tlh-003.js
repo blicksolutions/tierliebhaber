571bc54e08d06af4be0845f037d95a02c0743454
Sanaxo <manuel@blicksolutions.de>
2024-05-22 10:48:13 +0200

    remove tlh-003

--- assets/bs-ab-tlh-003.js
+++
@@ -1,8 +0,0 @@
-window.activateAbTlh003 = () => {
-    const targetCollection = document.querySelector('[data-section-type="collection"]');
-    const targetCallout = document.querySelector('.subscriptionReminderCallout');
-
-    if (targetCallout && targetCollection) {
-        targetCallout.classList.add('active');
-    }
-}
