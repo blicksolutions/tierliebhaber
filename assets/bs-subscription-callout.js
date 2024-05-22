05eb6be1efa7b55da30c5cb80e5c4e2d34d9237f
Sanaxo <manuel@blicksolutions.de>
2024-05-22 10:08:08 +0200

    add feature

---
+++ assets/bs-subscription-callout.js
@@ -0,0 +1,8 @@
+(() => {
+    const targetCollection = document.querySelector('[data-section-type="collection"]');
+    const targetCallout = document.querySelector('.subscriptionReminderCallout');
+
+    if (targetCallout && targetCollection) {
+        targetCallout.classList.add('active');
+    }
+})()
