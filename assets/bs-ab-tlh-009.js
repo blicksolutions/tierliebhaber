9fb0e230909b2f00517e12be28efb1a19a2f4cee
Sanaxo <manuel@blicksolutions.de>
2024-05-27 13:25:57 +0200

    add ab code

---
+++ assets/bs-ab-tlh-009.js
@@ -0,0 +1,33 @@
+window.activateAbTlh009 = () => {
+	console.log('activateAbTlh009');
+	const mutationObserver = new MutationObserver((entries) => {
+		entries.forEach((entry) => {
+			if (entry.target.classList.contains('rc-widget-injection-parent')) {
+				console.log('widget');
+				const subscroptionOption = document.querySelector('.rc-option__subsave[data-option-subsave]');
+				if (!subscroptionOption) return;
+
+				if (window.Shopify.isUserLoggedIn == true) {
+					setTimeout(() => {
+						subscroptionOption.click();
+					}, 100);
+					console.log('subscroptionOption');
+				}
+			}
+		});
+	});
+
+	const rechargeInjectionElement = document.querySelector('.rc-widget-injection-parent');
+
+	if (rechargeInjectionElement != undefined) {
+		mutationObserver.observe(rechargeInjectionElement, {
+			childList: true,
+			subtree: true,
+			attributes: true,
+		});
+	}
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+	window.activateAbTlh009();
+});
