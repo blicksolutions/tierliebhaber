78aa4ca95e29a6b4b84ea74975fa778e43fa2440
marcel <marcel@blicksolutions.de>
2024-06-11 11:56:21 +0200

    ab/tlh-020

---
+++ assets/bs-ab-tlh-020.js
@@ -0,0 +1,25 @@
+window.activateAbTlh020 = () => {
+    const payLaterSnippet = document.querySelector('.paylater-snippet');
+
+    if (payLaterSnippet != undefined) {
+        payLaterSnippet.setAttribute('test-active', 'true');
+
+        const mutationObserver = new MutationObserver((entries) => {
+            entries.forEach((entry) => {
+                const newPrice = parseFloat(document.querySelector('.ProductMeta__PriceList .ProductMeta__Price').textContent.replace('€', '').replace(',', '.'));
+                payLaterSnippet.dataset.ppAmount = newPrice;
+            });
+        });
+
+        const priceMutationEl = document.querySelector('.ProductMeta__PriceList');
+
+        if (priceMutationEl != undefined) {
+            mutationObserver.observe(priceMutationEl, {
+                childList: true,
+                subtree: true
+            });
+        }
+    }
+};
+
+document.addEventListener('DOMContentLoaded', window.activateAbTlh020);
