41e4f509b23c314fabb1f22d8a77ca8cca7f9dc9
marcel <marcel@blicksolutions.de>
2024-06-14 09:33:57 +0200

    ab/tlh-024

--- assets/bs-ab-tlh-024.js
+++ assets/bs-ab-tlh-024.js
@@ -2,5 +2,3 @@ window.activateAbTlh024 = () => {
     const cartDrawer = document.querySelector('#sidebar-cart');
     cartDrawer.setAttribute('data-tlh-024', 'true');
 };
-
-document.addEventListener('DOMContentLoaded', window.activateAbTlh024);
