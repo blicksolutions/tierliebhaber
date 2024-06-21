4c50e1d2f00a54f391bad40e9a8aecf5f5d72f34
Marcel <34421293+mungemach@users.noreply.github.com>
2024-06-21 09:32:22 +0200

    Update assets/bs-ab-tlh-008.js

--- assets/bs-ab-tlh-008.js
+++ assets/bs-ab-tlh-008.js
@@ -1,6 +1,5 @@
 window.activateAbTlh008 = () => {
 	const pageContainer = document.querySelector('.PageContainer');
-	console.log('pageContainer', pageContainer);
 
 	pageContainer.setAttribute('data-js-tlh-008', 'true');
 };
