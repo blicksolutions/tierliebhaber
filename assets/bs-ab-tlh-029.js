fa3635ecf564a081a2d01661b302616eeaebc6a9
Sanaxo <manuel@blicksolutions.de>
2024-06-25 13:37:06 +0200

    add metafield

--- assets/bs-ab-tlh-029.js
+++ assets/bs-ab-tlh-029.js
@@ -1,7 +1,11 @@
 window.activateAbTlh029 = () => {
-	const bestSellerItem = document.querySelector('.CollectionInner__Products .ProductItem__Wrapper.ProductBadge__Bestseller');
-	console.log('bestSellerItem', bestSellerItem);
-	if (!bestSellerItem) return;
+	const bestsellerProductItemId = window.Shopify.collectionBestsellerProductId;
+	if (!bestsellerProductItemId) return;
+
+	const bestSellerProductCard = document.querySelector(`.CollectionInner__Products .ProductItem[data-js-product-item="${bestsellerProductItemId}"]`);
+	if (!bestSellerProductCard) return;
+
+	const bestSellerItem = bestSellerProductCard.querySelector('.ProductItem__Wrapper.ProductBadge__Bestseller');
 
 	bestSellerItem.setAttribute('data-js-tlh-029', 'true');
 };
