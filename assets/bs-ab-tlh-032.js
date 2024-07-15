44b2a9a639b8b940657d5722c8b2f3b045d534d1
Antony Schmidt <antonyjonah@gmail.com>
2024-07-15 14:49:16 +0200

    add custom class to ab and style accordingley in css

---
+++ assets/bs-ab-tlh-032.js
@@ -0,0 +1,7 @@
+window.activateAbTlh032 = () => {
+	document.querySelector(".AnnouncementBar")?.parentElement.classList.add("announcement-bar--variation-b");
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    window.activateAbTlh032();
+})
