c09648ff1c0265630122881a893f2dd3d8ec405c
Antony Schmidt <antonyjonah@gmail.com>
2024-07-16 12:30:10 +0200

    load in extra flickity slider and adjust classes for visibility

--- assets/bs-ab-tlh-033.js
+++ assets/bs-ab-tlh-033.js
@@ -1,5 +1,6 @@
 window.activateAbTlh033 = () => {
-	
+	document.querySelector(".Product__SlideshowMobileNav").classList.add("Product__SlideshowMobileNav--hidden");
+	document.querySelector(".Product__SlideshowMobileNav--variation-b").style.display = "block";
 };
 
 document.addEventListener("DOMContentLoaded", () => {
