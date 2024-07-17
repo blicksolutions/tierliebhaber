80af08f6c9becc71013e3705d284405f99ac499e
Antony Schmidt <antonyjonah@gmail.com>
2024-07-17 12:34:00 +0200

    remove call function

--- assets/bs-ab-tlh-032.js
+++ assets/bs-ab-tlh-032.js
@@ -1,7 +1,3 @@
 window.activateAbTlh032 = () => {
 	document.querySelector(".AnnouncementBar")?.parentElement.classList.add("announcement-bar--variation-b");
 };
-
-window.addEventListener("DOMContentLoaded", () => {
-    window.activateAbTlh032();
-})
