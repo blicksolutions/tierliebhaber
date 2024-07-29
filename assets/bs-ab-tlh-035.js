859d0a04eae90a89d2cfc68d7b548afc99809b87
Sanaxo <manuel@blicksolutions.de>
2024-07-29 12:29:41 +0200

    adjust script

--- assets/bs-ab-tlh-035.js
+++ assets/bs-ab-tlh-035.js
@@ -1,19 +1,23 @@
 window.activateAbTlh035 = () => {
 	const productMeta = document.querySelector('.ProductMeta');
-	const pdpStockBar = document.querySelector('.pdp-stock-bar');
-	const taxNotice = document.querySelector('.ProductMeta__TaxNotice');
-	const reviews = productMeta.querySelector('.jdgm-widget');
-	const desktopLikeBadge = productMeta.querySelector('.LikeBadge.hidden-pocket.-Initialized');
+	const isBestseller = productMeta.getAttribute('data-js-bestseller');
 
-	const newStockBar = pdpStockBar.cloneNode(true);
-	const newdesktopLikeBadge = desktopLikeBadge.cloneNode(true);
-	newdesktopLikeBadge.classList.add('tlh-035');
-	pdpStockBar.remove();
-	reviews.remove();
-	taxNotice.after(newStockBar);
-	taxNotice.after(newdesktopLikeBadge);
+	if (isBestseller == 'true') {
+		const pdpStockBar = document.querySelector('.pdp-stock-bar');
+		const taxNotice = document.querySelector('.ProductMeta__TaxNotice');
+		const reviews = productMeta.querySelector('.jdgm-widget');
+		const desktopLikeBadge = productMeta.querySelector('.LikeBadge.hidden-pocket.-Initialized');
 
-	productMeta.setAttribute('data-js-tlh-035', true);
+		const newStockBar = pdpStockBar.cloneNode(true);
+		const newdesktopLikeBadge = desktopLikeBadge.cloneNode(true);
+		newdesktopLikeBadge.classList.add('tlh-035');
+		pdpStockBar.remove();
+		reviews.remove();
+		taxNotice.after(newStockBar);
+		taxNotice.after(newdesktopLikeBadge);
+
+		productMeta.setAttribute('data-js-tlh-035', true);
+	} else return;
 };
 
 document.addEventListener('DOMContentLoaded', window.activateAbTlh035);
