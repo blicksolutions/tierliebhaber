7afb56060bc6958ebb264a87428f140ba1884748
Sanaxo <manuel@blicksolutions.de>
2024-07-30 13:11:40 +0200

    add test code

---
+++ assets/bs-ab-tlh-039.js
@@ -0,0 +1,6 @@
+window.activateAbTlh039 = () => {
+	const pageContainer = document.querySelector('.PageContainer');
+
+	pageContainer.setAttribute('data-js-tlh-039', 'true');
+};
+document.addEventListener('DOMContentLoaded', window.activateAbTlh039);
