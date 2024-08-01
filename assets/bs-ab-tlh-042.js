d9d0d4f2b659e5eb3cba3c5ca42e204f4085f586
Sanaxo <manuel@blicksolutions.de>
2024-08-01 15:03:36 +0200

    Update bs-ab-tlh-042.js

--- assets/bs-ab-tlh-042.js
+++ assets/bs-ab-tlh-042.js
@@ -1,5 +1,7 @@
 window.activateAbTlh042 = () => {
 	const pagecontainerElement = document.querySelector('.PageContainer');
+	const headerElement = document.querySelector('#shopify-section-header');
 	pagecontainerElement.setAttribute('data-js-tlh-042', 'true');
+	headerElement.style.top = '0 !important';
 };
 document.addEventListener('DOMContentLoaded', window.activateAbTlh042);
