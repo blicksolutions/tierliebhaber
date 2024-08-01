552961eeff10d9761c6c4b95064509d09df96660
Sanaxo <manuel@blicksolutions.de>
2024-08-01 11:32:13 +0200

    adjust script

--- assets/bs-ab-tlh-045.js
+++ assets/bs-ab-tlh-045.js
@@ -1,22 +1,29 @@
 const changeShippingColor = () => {
 	setTimeout(() => {
 		const shippingCostElement = document.querySelector('.Drawer__Footer__Delivery');
+		console.log('shippingCostElement', shippingCostElement);
+		const subTotalElement = document.querySelector('.Drawer__Footer__SubtotalPrice');
+		console.log('subTotalElement', subTotalElement);
+		if (!subTotalElement) return;
+
+		const price = parseFloat(subTotalElement.getAttribute('data-price').replace('€', '').replace(',', '.'));
+		console.log('price', price);
 
 		switch (window.Shopify.locale) {
 			case 'de':
-				if (window.cartTotal > 4900) {
+				if (price > 49.0) {
 					shippingCostElement.setAttribute('data-js-tlh-045', 'true');
 				}
 				break;
 
 			case 'at':
-				if (window.cartTotal > 6900) {
+				if (price > 69.0) {
 					shippingCostElement.setAttribute('data-js-tlh-045', 'true');
 				}
 				break;
 
 			case 'ch':
-				if (window.cartTotal > 12900) {
+				if (price > 129.0) {
 					shippingCostElement.setAttribute('data-js-tlh-045', 'true');
 				}
 				break;
