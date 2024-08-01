ab25c66c461a06ef9bcb8fbeb93211150849da22
Antony Schmidt <antonyjonah@gmail.com>
2024-08-01 15:28:18 +0200

    add version b html and style

---
+++ assets/bs-ab-tlh-037.js
@@ -0,0 +1,7 @@
+window.activateAbTlh037 = () => {
+    document.querySelector(".Slideshow--versionA").style.display = "none";
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    window.activateAbTlh037();
+})
