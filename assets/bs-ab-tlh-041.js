d8fa271a359c7037250c2089315ad2f5563c94b9
Sanaxo <manuel@blicksolutions.de>
2024-08-02 10:49:33 +0200

    add test code

---
+++ assets/bs-ab-tlh-041.js
@@ -0,0 +1,20 @@
+window.activateAbTlh041 = () => {
+	function reformatPrice(text) {
+		console.log('text', text);
+		return text.replace(/€\s*([0-9]+(?:[.,][0-9]{1,3})?(?:[.,][0-9]{2})?)/g, '$1€');
+	}
+
+	function reformatPricesInNode(node) {
+		if (node.nodeType === Node.TEXT_NODE) {
+			node.nodeValue = reformatPrice(node.nodeValue);
+		} else if (node.nodeType === Node.ELEMENT_NODE) {
+			node.childNodes.forEach(reformatPricesInNode);
+		}
+	}
+
+	reformatPricesInNode(document.body);
+
+	document.addEventListener('variant:changed', () => {
+		reformatPricesInNode(document.body);
+	});
+};
