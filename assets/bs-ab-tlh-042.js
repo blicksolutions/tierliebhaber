7dcbc000df96975b9a063ca4d1b52bdfeff7a4a9
Sanaxo <manuel@blicksolutions.de>
2024-08-26 10:25:12 +0200

    remove test code

--- assets/bs-ab-tlh-042.js
+++
@@ -1,6 +0,0 @@
-window.activateAbTlh042 = () => {
-	const pagecontainerElement = document.querySelector('.PageContainer');
-	const headerElement = document.querySelector('#shopify-section-header');
-	pagecontainerElement.setAttribute('data-js-tlh-042', 'true');
-	headerElement.setAttribute('style', 'top: 0 !important;');
-};
