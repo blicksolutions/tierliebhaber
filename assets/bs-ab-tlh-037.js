697fd7419546132999d5cb125cf089bdf36a620e
Antony Schmidt <antonyjonah@gmail.com>
2024-09-02 12:04:50 +0200

    hide vers B on default and adjust visibility on function call

--- assets/bs-ab-tlh-037.js
+++ assets/bs-ab-tlh-037.js
@@ -1,3 +1,4 @@
 window.activateAbTlh037 = () => {
-    document.querySelector(".Slideshow--versionA").style.display = "none";
+    document.querySelector(".Slideshow--versionA")?.style.display = "none";
+    document.querySelector(".Slideshow--versionB")?.style.display = "flex";
 };
