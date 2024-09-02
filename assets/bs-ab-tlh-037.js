2f02d4174cd68f63cb8e9a40e58317d10eacd03a
Antony Schmidt <antonyjonah@gmail.com>
2024-09-02 11:58:19 +0200

    rm function call on dom loaded

--- assets/bs-ab-tlh-037.js
+++ assets/bs-ab-tlh-037.js
@@ -1,7 +1,3 @@
 window.activateAbTlh037 = () => {
     document.querySelector(".Slideshow--versionA").style.display = "none";
 };
-
-document.addEventListener("DOMContentLoaded", () => {
-    window.activateAbTlh037();
-})
