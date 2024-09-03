27959cf0507dc91d00d473f3d817d900113d964f
Antony Schmidt <antonyjonah@gmail.com>
2024-09-03 16:08:42 +0200

    rm function call on dom load

--- assets/bs-ab-tlh-049.js
+++ assets/bs-ab-tlh-049.js
@@ -1,7 +1,3 @@
 window.activateAbTlh049 = () => {
     document.querySelector(".bs-info-banner").style.display = "flex";
 };
-
-document.addEventListener("DOMContentLoaded", () => {
-    window.activateAbTlh049();
-})
