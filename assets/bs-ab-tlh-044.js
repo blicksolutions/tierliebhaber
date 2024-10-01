620fe978371b3c4a9e09805e8f6849c4a66c3b07
mahe2810 <martin@blicksolutions.de>
2024-10-01 17:11:26 +0200

    added close and open functionality

--- assets/bs-ab-tlh-044.js
+++ assets/bs-ab-tlh-044.js
@@ -1,5 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+	/******************************************************************/
+	/*  open/close submenu
+	/******************************************************************/
+
+	const openSubmenu = () => {
+		const subMenuItems = document.querySelectorAll("li.SidebarMenu__has-submenu");
+
+		subMenuItems.forEach((subMenuItem) => {
+			subMenuItem.addEventListener("click", () => {
+				subMenuItem.querySelector(".SidebarMenu__submenu").classList.add("SidebarMenu__submenu--open")
+			});	
+		})
+	}
+
+	const closeSubmenu = () => {
+		const closeSubmenus = document.querySelectorAll(".SidebarMenu__submenu-close");
+
+		closeSubmenus.forEach((closeSubmenu) => {
+			closeSubmenu.addEventListener("click", (event) => {
+
+				event.stopPropagation();
+				closeSubmenu.parentNode.classList.remove("SidebarMenu__submenu--open")
+			});	
+		})
+	}
+
+
+	/******************************************************************/
+	/* open sidebar vavigation
+	/******************************************************************/
+
     const openSideBarNavigation = () => {
 		const sideBarAVariant = document.querySelector(".a-variant.SidebarMenu.Drawer");
         const sidebarBVariant = document.querySelector(".tlh-044.SidebarMenu.Drawer");
@@ -18,6 +49,8 @@ document.addEventListener("DOMContentLoaded", () => {
     
     window.activateAbTlh044 = () => {
         openSideBarNavigation();
+		openSubmenu();
+		closeSubmenu();
     };
     
     window.activateAbTlh044();
