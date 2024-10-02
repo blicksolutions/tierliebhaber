03b46ff9a438468cd5b67e897ad11e45c5cbf876
mahe2810 <martin@blicksolutions.de>
2024-10-02 14:54:22 +0200

    added bestseller slider + updatet menu

--- assets/bs-ab-tlh-044.js
+++ assets/bs-ab-tlh-044.js
@@ -1,57 +1,91 @@
 document.addEventListener("DOMContentLoaded", () => {
+	/******************************************************************/
+	/* open sidebar vavigation
+	/******************************************************************/
+
+	const bestsellerSwiper = () => {
+		const bestseller = new Swiper(".bestseller__swiper", {
+			slidesPerView: 1,
+			centerInsufficientSlides: true,
+			spaceBetween: 10,
+		});
+
+		const bestsellerCategories = document.querySelectorAll(".bestseller__category");
+
+		bestsellerCategories.forEach((bestsellerCategory) => {
+			bestsellerCategory.addEventListener("click", () => {
+
+				bestsellerCategories.forEach((category) => {
+					category.classList.remove("active");
+				});
+
+				bestsellerCategory.classList.add("active");
+
+				bestseller.slideTo(bestsellerCategory.dataset.slideTo);
+			});
+		});
+	};
 
 	/******************************************************************/
-	/*  open/close submenu
+	/*  open submenu
 	/******************************************************************/
 
 	const openSubmenu = () => {
-		const subMenuItems = document.querySelectorAll("li.SidebarMenu__has-submenu");
+		const subMenuItems = document.querySelectorAll("li.SidebarMenu__item-has-submenu");
+		const sideBarMenuDrawer = document.querySelector(".tlh-044.SidebarMenu");
+
+		
 
 		subMenuItems.forEach((subMenuItem) => {
 			subMenuItem.addEventListener("click", () => {
-				subMenuItem.querySelector(".SidebarMenu__submenu").classList.add("SidebarMenu__submenu--open")
-			});	
-		})
-	}
+				subMenuItem.querySelector(".SidebarMenu__submenu").classList.add("SidebarMenu__submenu--open");
+			});
+		});
+	};
+
+	/******************************************************************/
+	/*  close submenu
+	/******************************************************************/
 
 	const closeSubmenu = () => {
 		const closeSubmenus = document.querySelectorAll(".SidebarMenu__submenu-close");
+		const sideBarMenuDrawer = document.querySelector(".tlh-044.SidebarMenu");
 
 		closeSubmenus.forEach((closeSubmenu) => {
 			closeSubmenu.addEventListener("click", (event) => {
-
 				event.stopPropagation();
-				closeSubmenu.parentNode.classList.remove("SidebarMenu__submenu--open")
-			});	
-		})
-	}
 
+				closeSubmenu.parentNode.classList.remove("SidebarMenu__submenu--open");
+			});
+		});
+	};
 
 	/******************************************************************/
 	/* open sidebar vavigation
 	/******************************************************************/
 
-    const openSideBarNavigation = () => {
+	const openSideBarNavigation = () => {
 		const sideBarAVariant = document.querySelector(".a-variant.SidebarMenu.Drawer");
-        const sidebarBVariant = document.querySelector(".tlh-044.SidebarMenu.Drawer");
-        const headerIconBVariant = document.querySelector(".Header__Icon");
-        const drawerCloseBVariant = document.querySelector(".tlh-044 .Drawer__Close");
+		const sidebarBVariant = document.querySelector(".tlh-044.SidebarMenu.Drawer");
+		const headerIconBVariant = document.querySelector(".Header__Icon");
+		const drawerCloseBVariant = document.querySelector(".tlh-044 .Drawer__Close");
 
-        headerIconBVariant.addEventListener("click", () => {
+		headerIconBVariant.addEventListener("click", () => {
 			sideBarAVariant.style.display = "none";
-            sidebarBVariant.ariaHidden = false;
-        });
+			sidebarBVariant.ariaHidden = false;
+		});
 
-        drawerCloseBVariant.addEventListener("click", () => {
-            sidebarBVariant.ariaHidden = true;
-        })
-    }
-    
-    window.activateAbTlh044 = () => {
-        openSideBarNavigation();
+		drawerCloseBVariant.addEventListener("click", () => {
+			sidebarBVariant.ariaHidden = true;
+		});
+	};
+
+	window.activateAbTlh044 = () => {
+		openSideBarNavigation();
 		openSubmenu();
 		closeSubmenu();
-    };
-    
-    window.activateAbTlh044();
-})
+		bestsellerSwiper();
+	};
+
+	window.activateAbTlh044();
+});
