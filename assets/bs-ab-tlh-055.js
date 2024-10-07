a4e89fff50bfcb93163ff5da0aafb3302844cd56
Antony Schmidt <antonyjonah@gmail.com>
2024-10-07 11:53:07 +0200

    change content in section, add script that hides original and displays b version

--- assets/bs-ab-tlh-055.js
+++ assets/bs-ab-tlh-055.js
@@ -1,4 +1,11 @@
 window.activateAbTlh055 = () => {
+	const versionA = document.querySelector(".bs-hero-banner:first-of-type");
+	const versionB = document.querySelector(".bs-hero-banner.bs-hero-banner--version-b");
+
+	if(versionA && versionB) {
+		versionA.style.display = "none";
+		versionB.style.display = "flex";
+	}
 };
 
 document.addEventListener("DOMContentLoaded", () => {
