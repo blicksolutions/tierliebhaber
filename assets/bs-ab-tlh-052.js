043402fe759ddbe298cc5b3f322e4e95b23493de
Sanaxo <manuel@blicksolutions.de>
2024-10-08 09:52:53 +0200

    adjust liquid

--- assets/bs-ab-tlh-052.js
+++ assets/bs-ab-tlh-052.js
@@ -1,24 +1,14 @@
 window.activateAbTlh052 = () => {
 	const cartDrawer = document.querySelector('#sidebar-cart[data-js-tlh-052]')
-	console.log('cartDrawer', cartDrawer)
 	if (!cartDrawer) return
 
 	cartDrawer.setAttribute('data-js-tlh-052', 'true')
 
-	const mutationObserver = new MutationObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.target.classList.contains('Drawer__Main')) {
-				console.log('droggelbecher')
-            }
-        });
-    });
-
-	mutationObserver.observe(cartDrawer, {
-		childList: true,
-		subtree: true,
-		attributes: true
-	});
-    
 };
 
-document.addEventListener('DOMContentLoaded', window.activateAbTlh052)
+document.addEventListener('DOMContentLoaded',window.activateAbTlh052)
+
+const tlh052Click = (productId) => {
+	const productContainer = document.querySelector(`[data-js-tlh-052-product-container][data-js-product-id="${productId}"]`)
+	productContainer.classList.toggle('open')
+}
