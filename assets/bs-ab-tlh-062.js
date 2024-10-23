75ef11082b5ecaea5da15a11431ebcd6b9c03784
Antony Schmidt <antonyjonah@gmail.com>
2024-10-23 14:51:50 +0200

    create ab snippet and render at keypoint, hide other socialBadges adjust styles accordingly

---
+++ assets/bs-ab-tlh-062.js
@@ -0,0 +1,15 @@
+window.activateAbTlh062 = () => {
+		const socialProofsB = document.querySelectorAll(".LikeBadge--version-b");
+
+		if(socialProofsB.length > 0) {
+			socialProofsB.forEach((socialProofB) => {
+				socialProofB.classList.add("active");
+				socialProofB.style.display = "flex";
+			})
+		}
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	window.activateAbTlh062();
+})
+
