03eb47a67bcd2f58fbd53ce21e01bace89a3b122
Antony Schmidt <antonyjonah@gmail.com>
2024-10-31 04:43:58 +0100

    create amount info component

--- assets/bs-ab-tlh-060.js
+++ assets/bs-ab-tlh-060.js
@@ -1,9 +1,17 @@
 window.activateAbTlh060 = () => {
+    // anchor component
     const pdpAnchor = document.querySelector('[data-bs-tlh-060-anchor]');
 
     if(!pdpAnchor) return;
 
     pdpAnchor.setAttribute('data-bs-tlh-060-anchor', true)
+
+    // amount info component
+    const pdpAmountInfo = document.querySelector('[data-bs-tlh-060-amount-info]');
+
+    if(!pdpAmountInfo) return;
+
+    pdpAmountInfo.setAttribute('data-bs-tlh-060-amount-info', true)
 };
 
 document.addEventListener('DOMContentLoaded', () => {
