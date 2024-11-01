b0e3f1d05a33f01df8999c523cfcbf7752c09131
Antony Schmidt <antonyjonah@gmail.com>
2024-11-01 04:29:33 +0100

    add opening of popup and closing logic

--- assets/bs-ab-tlh-060.js
+++ assets/bs-ab-tlh-060.js
@@ -1,19 +1,42 @@
 window.activateAbTlh060 = () => {
     // anchor component
     const pdpAnchor = document.querySelector('[data-bs-tlh-060-anchor]');
+    const pdpAmountInfo = document.querySelector('[data-bs-tlh-060-amount-info]');
+    const popupContainer = document.querySelector('[data-bs-tlh-060-popup]');
+    const popupContent = document.querySelector('.bs-amount-calculator-popup');
+    const closeButton = document.querySelector('.bs-amount-calculator-popup__header');
 
     if(!pdpAnchor) return;
-
-    pdpAnchor.setAttribute('data-bs-tlh-060-anchor', true)
-
-    // amount info component
-    const pdpAmountInfo = document.querySelector('[data-bs-tlh-060-amount-info]');
-
     if(!pdpAmountInfo) return;
+    if(!popupContainer) return;
 
-    pdpAmountInfo.setAttribute('data-bs-tlh-060-amount-info', true)
+    pdpAnchor.setAttribute('data-bs-tlh-060-anchor', true);
+    pdpAmountInfo.setAttribute('data-bs-tlh-060-amount-info', true);
+    popupContainer.setAttribute('data-bs-tlh-060-popup', true);
 
-    // popup component
+    pdpAnchor.addEventListener('click', () => {
+        popupContainer.classList.add('active');
+        document.querySelector('html').style.overflow = 'hidden';
+    });
+ 
+    pdpAmountInfo.addEventListener('click', () => {
+        popupContainer.classList.add('active');
+        document.querySelector('html').style.overflow = 'hidden';
+    });
+
+    closeButton.addEventListener('click', () => {
+        popupContainer.classList.remove('active');
+        document.querySelector('html').style.overflow = 'unset';
+    });
+
+    popupContainer.addEventListener('click', (e) => {
+        if(!popupContent.contains(e.target)) {
+            popupContainer.classList.remove('active');
+            document.querySelector('html').style.overflow = 'unset';
+        }
+    });
+
+    // popup progressbar
     const progressbarActiveThumb = document.querySelector('.bs-amount-calculator-popup__progress-bar__thumb-active');
     const bullets = document.querySelectorAll('.bs-amount-calculator-popup__progress-bar__bullet');
     let currentActiveBullet = document.querySelector('.bs-amount-calculator-popup__progress-bar__bullet.active')
@@ -22,7 +45,7 @@ window.activateAbTlh060 = () => {
         bullet.addEventListener('click', (e) => {
             if(e.currentTarget != currentActiveBullet) {
                 currentActiveBullet.classList.remove('active');
-                progressbarActiveThumb.attributes.style.value = `width: ${e.currentTarget.dataset.activeThumbWidth}%;`;
+                progressbarActiveThumb.style.width = `${e.currentTarget.dataset.activeThumbWidth}%`;
                 e.currentTarget.classList.add('active');
                 currentActiveBullet = e.currentTarget;
             }
