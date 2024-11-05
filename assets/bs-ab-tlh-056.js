ea3f22452b69841745e257191429c6825706f0b6
Sanaxo <manuel@blicksolutions.de>
2024-11-05 18:56:14 +0100

    add test code

---
+++ assets/bs-ab-tlh-056.js
@@ -0,0 +1,47 @@
+window.activateAbTlh056 = (testVariant) => {
+	const cartDrawerElement = document.querySelector('[data-js-tlh-056]')
+	if (!cartDrawerElement) return
+
+	cartDrawerElement.setAttribute('data-js-tlh-056', testVariant)
+
+	if (testVariant == 'b') {
+
+		const mutationObserverPrice = new MutationObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.type == 'childList') {
+					if (entry.target.classList.contains('Badge__Savings')) return
+						const cartSavingsElement = document.querySelector('[tlh-056-cart-savings]')
+						if (!cartSavingsElement) return
+		
+						const cartSavings = cartSavingsElement.getAttribute('tlh-056-cart-savings')
+	
+						const badgeSavingsElement = document.querySelector('[data-js-tlh-056-badge-savings]')
+						if (badgeSavingsElement.innerText != cartSavings) {
+							badgeSavingsElement.innerText = cartSavings
+						}
+					
+				}
+			});
+		});
+	
+		const cartDrawer= document.querySelector('#shopify-section-cart-drawer')
+	
+		if (cartDrawer != undefined) {
+			mutationObserverPrice.observe(cartDrawer, {
+				childList: true,
+				subtree: true,
+				attributes: true,
+			});
+		}
+	}
+
+};
+
+document.addEventListener('DOMContentLoaded',() => {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	const variant = urlParams.get('testvariant')
+	console.log('variant', variant)
+
+	window.activateAbTlh056(variant)
+})
