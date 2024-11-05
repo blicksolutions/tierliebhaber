575fa8c9367244ab1d6be07fd19bc407bf7815f5
Antony Schmidt <antonyjonah@gmail.com>
2024-11-05 11:43:11 +0100

    initial commit, create empty files and reference in theme liquid

---
+++ assets/bs-ab-tlh-064.js
@@ -0,0 +1,12 @@
+window.activateAbTlh064B = () => {
+	
+}
+
+window.activateAbTlh064C = () => {
+	
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	window.activateAbTlh064B();
+    window.activateAbTlh064C();
+})
