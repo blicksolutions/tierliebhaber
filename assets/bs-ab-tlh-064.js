992ba33e6809a5423bbeac1c3eac7380dca27c2a
Antony Schmidt <antonyjonah@gmail.com>
2024-11-06 07:05:25 +0100

    adjust specific bestseller links and replace product images / attributes with ones from nex product and adjust in css

--- assets/bs-ab-tlh-064.js
+++ assets/bs-ab-tlh-064.js
@@ -9,16 +9,33 @@ const adjustHolidayCards = () => {
     desktopMenuItemsWrapper.setAttribute('data-ab-tlh-064', true);
 }
 
-const content = (linkUrl, imgUrl, title) => { 
-    return `<a class="bestseller__link"  href="${linkUrl}"><img class="bestseller__image" src="${imgUrl}" alt="${title}">${title}</a>`;
+const replaceBestsellerLinkAttributes = (target, linkUrl, imgUrl, title) => {
+    if(target) {
+        target.setAttribute('href', linkUrl);
+        target.setAttribute('data-ab-tlh-064', true);
+        target.classList.remove('bestseller__link--highlight');
+        target.innerHTML = `<img class="bestseller__image" src="${imgUrl}" alt="${title}"><span>${title}</span>`
+    }
 };
 
 window.activateAbTlh064B = () => {
-	adjustHolidayCards();
+	// adjustHolidayCards();
 }
 
 window.activateAbTlh064C = () => {
-	
+    const zSnackTarget = document.querySelector('.bestseller__link:has(.bestseller__image[alt="Z-Snack"])');
+    const darmwohlTarget = document.querySelector('.bestseller__link:has(.bestseller__image[alt="Darmwohl Sticks"])');
+    const grasKotfresserTarget = document.querySelector('.bestseller__link:has(.bestseller__image[alt="Gras- & Kotfresser Drops"])');
+    const darmpflegeTarget = document.querySelector('.bestseller__link:has(.bestseller__image[alt="Darmpflege Drops"])');
+    const bestsellerTitle = document.querySelector('.bestseller__title'); 
+
+    replaceBestsellerLinkAttributes(zSnackTarget, 'https://tierliebhaber.de/collections/z-produkte/products/z-bundle-zb', 'https://tierliebhaber.de/cdn/shop/files/bundle1_9e3d58a5-cd0d-483a-9773-b3f03634ebcb_1200x.jpg?v=1716366913', 'Z Bundle');
+    replaceBestsellerLinkAttributes(darmwohlTarget, 'https://tierliebhaber.de/collections/zahnpflege/products/zahnpflege-bundle', 'https://tierliebhaber.de/cdn/shop/files/20240705_Tierliebhaber2448_1200x.jpg?v=1724063840', 'Zahnpflege');
+    replaceBestsellerLinkAttributes(grasKotfresserTarget, 'https://tierliebhaber.de/collections/darmpflege/products/sodbrennen-bundle', 'https://tierliebhaber.de/cdn/shop/files/SodbrennenBundle_1000x.jpg?v=1713261808', 'Sodbrennen');
+    replaceBestsellerLinkAttributes(darmpflegeTarget, 'https://tierliebhaber.de/collections/darmpflege/products/analdrusen-bundle', 'https://tierliebhaber.de/cdn/shop/files/analdruesen_bundle_gzg_1000x.jpg?v=1717012761', 'Analdrüsen');
+
+	adjustHolidayCards();
+    bestsellerTitle.innerHTML = 'Bundles & Bestseller';
 }
 
 document.addEventListener("DOMContentLoaded", () => {
