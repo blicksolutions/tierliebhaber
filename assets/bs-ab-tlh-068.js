ed09aac16c5a32eb71dfe0cc26953a4bd6bbe7b3
Antony Schmidt <antonyjonah@gmail.com>
2024-11-19 12:01:53 +0100

    create new years banner and load in new upsell items, toggle visibility for each experiment

---
+++ assets/bs-ab-tlh-068.js
@@ -0,0 +1,20 @@
+window.activateAbTlh068B = () => {
+const cartDrawer = document.getElementById('sidebar-cart');
+
+if(!cartDrawer) return;
+
+cartDrawer.setAttribute('data-bs-tlh-068b', "");
+};
+
+window.activateAbTlh068C = () => {
+    const cartDrawer = document.getElementById('sidebar-cart');
+
+    if(!cartDrawer) return;
+    
+    cartDrawer.setAttribute('data-bs-tlh-068c', "");
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    // window.activateAbTlh068C();
+})
+
