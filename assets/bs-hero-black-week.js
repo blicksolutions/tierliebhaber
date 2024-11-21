5db50b775a6216306d3da836286063538030c6c0
mahe2810 <martin@blicksolutions.de>
2024-11-21 15:37:16 +0100

    added basic html structure

--- assets/bs-hero-black-week.js
+++ assets/bs-hero-black-week.js
@@ -1,3 +1,24 @@
 (() => {
+	document.addEventListener("DOMContentLoaded", () => {
+
+	});
+
+	/******************************************************************/
+	/* bestseller swiper
+	/******************************************************************/
+
+	const blackWeekSwiper = () => {
+		const blackWeek = new Swiper(".bestseller__swiper", {
+			slidesPerView: 5,
+			centerInsufficientSlides: true,
+			spaceBetween: 10,
+			allowTouchMove: false,
+		});
+	};
+
+	/******************************************************************/
+	/* counter
+	/******************************************************************/
+
 
 })();
