9d8a6cbd3290a69ada95b79767ca49a53de0fdf4
Sanaxo <manuel@blicksolutions.de>
2024-11-21 10:16:20 +0100

    adjust code

--- assets/bs-ab-tlh-069.js
+++ assets/bs-ab-tlh-069.js
@@ -1,6 +1,5 @@
 window.activateAbTlh069 = () => {
 	const announcementBar = document.querySelector('#shopify-section-announcement')
-	console.log('announcementBar', announcementBar)
 	if (!announcementBar) return
 
 	announcementBar.setAttribute('data-js-tlh-069', 'true')
