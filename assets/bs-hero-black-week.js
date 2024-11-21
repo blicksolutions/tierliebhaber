34e123ee30b6c089aed0b9ea14d0bb9bd7395baf
mahe2810 <martin@blicksolutions.de>
2024-11-21 21:38:08 +0100

    update

--- assets/bs-hero-black-week.js
+++ assets/bs-hero-black-week.js
@@ -1,6 +1,10 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
 
+
+		const countdownInterval = setInterval(updateCountdown, 1000);
+
+		updateCountdown();
 	});
 
 	/******************************************************************/
@@ -20,5 +24,28 @@
 	/* counter
 	/******************************************************************/
 
+	const targetDate = new Date('December 1, 2024 23:59:59').getTime();
+    const countDownCounterElement = document.querySelector('.bs-black-week-collection-banner__countdown__counter');
+
+    const updateCountdown = () => {
+        const now = new Date().getTime();
+        const timeRemaining = targetDate - now;
+
+        if (timeRemaining > 0) {
+            const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+            document.querySelector('.bs-black-week-collection-banner__countdown__counter__days').textContent = `${days}d`;
+            document.querySelector('.bs-black-week-collection-banner__countdown__counter__hours').textContent = `${hours}h`;
+            document.querySelector('.bs-black-week-collection-banner__countdown__counter__minutes').textContent = `${minutes}m`;
+            document.querySelector('.bs-black-week-collection-banner__countdown__counter__seconds').textContent = `${seconds}s`;
+        } else {
+            document.querySelector('.bs-black-week-collection-banner__countdown').innerHTML = '<span>Sale has ended!</span>';
+            clearInterval(countdownInterval);
+        }
+    }
+	
 
 })();
