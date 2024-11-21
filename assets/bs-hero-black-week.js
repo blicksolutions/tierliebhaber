a72e36459580d0edcfbc190cbdba92c8928f7227
mahe2810 <martin@blicksolutions.de>
2024-11-21 21:47:15 +0100

    update

--- assets/bs-hero-black-week.js
+++ assets/bs-hero-black-week.js
@@ -1,10 +1,33 @@
 (() => {
 	document.addEventListener("DOMContentLoaded", () => {
-
-
+		const targetDate = new Date('December 1, 2024 23:59:59').getTime();
+		const countDownCounterElement = document.querySelector('.bs-hero-black-week__countdown-counter');
+	
+		const updateCountdown = () => {
+			const now = new Date().getTime();
+			const timeRemaining = targetDate - now;
+	
+			if (timeRemaining > 0) {
+				const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+				const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+				const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+				const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+	
+				document.querySelector('.bs-hero-black-week__countdown-counter-days').textContent = `${days}d`;
+				document.querySelector('.bs-hero-black-week__countdown-counter-hours').textContent = `${hours}h`;
+				document.querySelector('.bs-hero-black-week__countdown-counter-minutes').textContent = `${minutes}m`;
+				document.querySelector('.bs-hero-black-week__countdown-counter-seconds').textContent = `${seconds}s`;
+			} else {
+				document.querySelector('.bs-hero-black-week__info-countdown').innerHTML = '<span>Sale has ended!</span>';
+				clearInterval(countdownInterval);
+			}
+		}
+	
 		const countdownInterval = setInterval(updateCountdown, 1000);
-
+	
 		updateCountdown();
+	
+		countDownCounterElement.classList.add('active');
 	});
 
 	/******************************************************************/
@@ -24,28 +47,5 @@
 	/* counter
 	/******************************************************************/
 
-	const targetDate = new Date('December 1, 2024 23:59:59').getTime();
-    const countDownCounterElement = document.querySelector('.bs-black-week-collection-banner__countdown__counter');
-
-    const updateCountdown = () => {
-        const now = new Date().getTime();
-        const timeRemaining = targetDate - now;
-
-        if (timeRemaining > 0) {
-            const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-            const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-            const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-
-            document.querySelector('.bs-black-week-collection-banner__countdown__counter__days').textContent = `${days}d`;
-            document.querySelector('.bs-black-week-collection-banner__countdown__counter__hours').textContent = `${hours}h`;
-            document.querySelector('.bs-black-week-collection-banner__countdown__counter__minutes').textContent = `${minutes}m`;
-            document.querySelector('.bs-black-week-collection-banner__countdown__counter__seconds').textContent = `${seconds}s`;
-        } else {
-            document.querySelector('.bs-black-week-collection-banner__countdown').innerHTML = '<span>Sale has ended!</span>';
-            clearInterval(countdownInterval);
-        }
-    }
-	
-
+ 
 })();
