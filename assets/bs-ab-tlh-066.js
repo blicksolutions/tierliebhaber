54a00e6c7ae5d5856c2098c68b743d7489402afb
Antony Schmidt <antonyjonah@gmail.com>
2024-12-11 12:53:52 +0100

    add custom ab blocks and load in template and toggle class for accordion effect

--- assets/bs-ab-tlh-066.js
+++ assets/bs-ab-tlh-066.js
@@ -23,7 +23,17 @@ window.activateAbTlh066 = () => {
         });
     }
 
+    const initiateFooterAccordions = () => {
+        const targetFooterBlocks = Array.from(document.querySelectorAll(".Footer__Block.Footer__Block--links h2"));
+        targetFooterBlocks.push(document.querySelector(".Footer__Block.Footer__Block--reachability h2"));
+
+        targetFooterBlocks.forEach((targetFooterBlock) => {
+            targetFooterBlock.addEventListener("click", (e) => e.currentTarget.parentElement.classList.toggle("active"));
+        })
+    }
+
     initiateKeypoints();
+    initiateFooterAccordions();
 };
 
 document.addEventListener("DOMContentLoaded", () => {
