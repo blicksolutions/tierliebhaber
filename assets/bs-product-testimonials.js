6d59d8ba04b83e462c2d5c61f93fe5e219731efd
Antony Schmidt <antonyjonah@gmail.com>
2024-12-18 13:07:12 +0100

    add section in template file and query select necessary reviews io snippet elements

--- assets/bs-product-testimonials.js
+++ assets/bs-product-testimonials.js
@@ -1,129 +1,121 @@
 (() => {
-	const reviewsContainer = document.querySelector('#judgeme_product_reviews');
-	if(!reviewsContainer) return;
+	setTimeout(() => {
+		const reviewsContainer = document.querySelector('reviewsio-product-reviews-widget');
+		if(!reviewsContainer) return;
+	
+		const reviews = reviewsContainer.querySelectorAll('.ElementsWidget__list .R-ContentList__item');	
+		const testimonialsWrapper = document.querySelector('[data-js-product-testimonials-wrapper]');
+	
+		if(!reviews.length) return;
+	
+		const reviewWidget = document.querySelector('.ElementsWidget__inner .header__inner .header__group:first-of-type');
+		const avgRating = reviewWidget.querySelector('div:first-of-type').innerText;
+		const reviewsNumber = reviewWidget.querySelector('div:last-of-type').innerText.match(/[\d,]+/)?.[0].replace(',', '.');
+		const avgRatingTarget = document.querySelector('.bs-product-testimonials__rating-product__average span');
+		const reviewsNumberTarget = document.querySelector('.bs-product-testimonials__rating-product__reviews-number span');
+		const starRatingWidget = reviewWidget.querySelector('div:nth-of-type(2) .R-RatingStars').outerHTML;
+		const starRatingWidgetTarget = document.querySelector('.bs-product-testimonials__rating-product');
+	
+		if(starRatingWidgetTarget && avgRatingTarget && reviewsNumberTarget){
+			starRatingWidgetTarget.insertAdjacentHTML('afterbegin', starRatingWidget);
+			avgRatingTarget.innerHTML = avgRating;
+			reviewsNumberTarget.innerHTML = reviewsNumber;
+		}
+	
+		const effectMetric = document.querySelector('.bs-product-testimonials__rating-metric[data-js-product-testimonials-metric-effect] .bs-product-testimonials__rating-metric__number');
+		const effectMetricRatingThumb = document.querySelector('.bs-product-testimonials__rating-metric[data-js-product-testimonials-metric-effect] .bs-product-testimonials__rating-metric__thumb');
+		const qualityMetric = document.querySelector('.bs-product-testimonials__rating-metric[data-js-product-testimonials-metric-quality] .bs-product-testimonials__rating-metric__number');
+		const qualityMetricRatingThumb = document.querySelector('.bs-product-testimonials__rating-metric[data-js-product-testimonials-metric-quality] .bs-product-testimonials__rating-metric__thumb');
+		const shippingMetric = document.querySelector('.bs-product-testimonials__rating-metric[data-js-product-testimonials-metric-shipping] .bs-product-testimonials__rating-metric__number');
+		const shippingMetricRatingThumb = document.querySelector('.bs-product-testimonials__rating-metric[data-js-product-testimonials-metric-shipping] .bs-product-testimonials__rating-metric__thumb');
+	
+		switch (avgRating * 1) {
+			case 5.00:
+				effectMetric.innerHTML = avgRating;
+				qualityMetric.innerHTML = avgRating;
+				shippingMetric.innerHTML = avgRating;
+				break;
+			case 4.99:
+				effectMetric.innerHTML = (avgRating - 0.02).toFixed(2);
+				qualityMetric.innerHTML = (avgRating * 1 + 0.01).toFixed(2);
+				shippingMetric.innerHTML = avgRating;
+				break;
+			default:
+				effectMetric.innerHTML = (avgRating - 0.03).toFixed(2);
+				qualityMetric.innerHTML = (avgRating * 1 + 0.02).toFixed(2);
+				shippingMetric.innerHTML = (avgRating * 1 + 0.01).toFixed(2);
+		}
+	
+		if(effectMetricRatingThumb && qualityMetricRatingThumb && shippingMetricRatingThumb) {
+			effectMetricRatingThumb.style.width = (effectMetric.innerHTML / 5 * 100) + '%';
+			qualityMetricRatingThumb.style.width = (qualityMetric.innerHTML / 5 * 100) + '%';
+			shippingMetricRatingThumb.style.width = (shippingMetric.innerHTML / 5 * 100) + '%';
+		}
+	
+		reviews.forEach((review) => {
+			const reviewSlide = document.createElement('div');
 
-	const reviews = reviewsContainer.querySelectorAll('.jdgm-widget .jdgm-rev-widg .jdgm-rev-widg__reviews .jdgm-rev');	
-	const testimonialsWrapper = document.querySelector('[data-js-product-testimonials-wrapper]');
-	if(!reviews.length) return;
+			reviewSlide.classList.add('bs-product-testimonials__card');
+			reviewSlide.classList.add('grid-item');
 
-	const reviewWidget = document.querySelector('.jdgm-rev-widg');
-	const avgRating = reviewWidget.dataset.averageRating;
-	const reviewsNumber = reviewWidget.dataset.numberOfReviews;
-	const avgRatingTarget = document.querySelector('.bs-product-testimonials__rating-product__average span');
-	const reviewsNumberTarget = document.querySelector('.bs-product-testimonials__rating-product__reviews-number span');
-	const starRatingWidget = document.querySelector('.jdgm-rev-widg__summary-stars').outerHTML;
-	const starRatingWidgetTarget = document.querySelector('.bs-product-testimonials__rating-product');
+			const icon = review.querySelector('.R-AvatarThumbnail__inner').outerHTML;
+			const rating = review.querySelector('.R-RatingStars').innerHTML;
+			const author = review.querySelector('.cssVar-authorName').innerText;
+			const text = review.querySelector('.item__inner div div div div + div:nth-of-type(3)').innerText;
+			const timestamp = review.querySelector('.item__inner div:nth-of-type(2)').innerText;
 
-	if(starRatingWidgetTarget && avgRatingTarget && reviewsNumberTarget){
-		starRatingWidgetTarget.insertAdjacentHTML('afterbegin', starRatingWidget);
-		avgRatingTarget.innerHTML = avgRating;
-		reviewsNumberTarget.innerHTML = reviewsNumber;
-	}
+			reviewSlide.innerHTML = `
+				<div class="testimonial__icon">
+					${icon}
+				</div>
+				<div class="testimonial__rating-time">
+					<div class="testimonial__rating">
+						${rating}
+					</div>
+					<div class="testimonial__time">
+						${timestamp}
+					</div>
+				</div>
+				<div class="testimonial__author-badge">
+					<div class="testimonial__author">
+						${author}
+					</div>
+					<div class="testimonial__badge">
+						Verifiziert
+					</div>
+				</div>
+				<div class="testimonial__text">
+					“${text}”
+				</div>
 
-	const effectMetric = document.querySelector('.bs-product-testimonials__rating-metric[data-js-product-testimonials-metric-effect] .bs-product-testimonials__rating-metric__number');
-	const effectMetricRatingThumb = document.querySelector('.bs-product-testimonials__rating-metric[data-js-product-testimonials-metric-effect] .bs-product-testimonials__rating-metric__thumb');
-	const qualityMetric = document.querySelector('.bs-product-testimonials__rating-metric[data-js-product-testimonials-metric-quality] .bs-product-testimonials__rating-metric__number');
-	const qualityMetricRatingThumb = document.querySelector('.bs-product-testimonials__rating-metric[data-js-product-testimonials-metric-quality] .bs-product-testimonials__rating-metric__thumb');
-	const shippingMetric = document.querySelector('.bs-product-testimonials__rating-metric[data-js-product-testimonials-metric-shipping] .bs-product-testimonials__rating-metric__number');
-	const shippingMetricRatingThumb = document.querySelector('.bs-product-testimonials__rating-metric[data-js-product-testimonials-metric-shipping] .bs-product-testimonials__rating-metric__thumb');
+			`;
 
-	switch (avgRating * 1) {
-		case 5.00:
-			effectMetric.innerHTML = avgRating;
-			qualityMetric.innerHTML = avgRating;
-			shippingMetric.innerHTML = avgRating;
-			break;
-		case 4.99:
-			effectMetric.innerHTML = (avgRating - 0.02).toFixed(2);
-			qualityMetric.innerHTML = (avgRating * 1 + 0.01).toFixed(2);
-			shippingMetric.innerHTML = avgRating;
-			break;
-		default:
-			effectMetric.innerHTML = (avgRating - 0.03).toFixed(2);
-			qualityMetric.innerHTML = (avgRating * 1 + 0.02).toFixed(2);
-			shippingMetric.innerHTML = (avgRating * 1 + 0.01).toFixed(2);
-	}
+			if(testimonialsWrapper) {
+				testimonialsWrapper.appendChild(reviewSlide);
 
-	if(effectMetricRatingThumb && qualityMetricRatingThumb && shippingMetricRatingThumb) {
-		effectMetricRatingThumb.style.width = (effectMetric.innerHTML / 5 * 100) + '%';
-		qualityMetricRatingThumb.style.width = (qualityMetric.innerHTML / 5 * 100) + '%';
-		shippingMetricRatingThumb.style.width = (shippingMetric.innerHTML / 5 * 100) + '%';
-	}
+				const grid = document.querySelector('.bs-product-testimonials__list.grid')
 
-	reviews.forEach((review) => {
-		const mutationObserver = new MutationObserver((entries) => {
-			entries.forEach((entry) => {
-				if (entry.attributeName == 'class' && entry.target.classList.contains('jdgm-rev__timestamp') && !entry.target.classList.contains('jdgm-spinner')) {
-					const reviewSlide = document.createElement('div');
-					reviewSlide.classList.add('bs-product-testimonials__card');
-					reviewSlide.classList.add('grid-item');
-
-					const icon = review.querySelector('.jdgm-rev__icon').outerHTML;
-					const rating = review.querySelector('.jdgm-rev__rating').outerHTML;
-					const author = review.querySelector('.jdgm-rev__author').innerText;
-					const text = review.querySelector('.jdgm-rev__body').innerText;
-					const timestamp = review.querySelector('.jdgm-rev__timestamp').innerText;
-
-					reviewSlide.innerHTML = `
-						<div class="testimonial__icon">
-							${icon}
-						</div>
-						<div class="testimonial__rating-time">
-							<div class="testimonial__rating">
-								${rating}
-							</div>
-							<div class="testimonial__time">
-								${timestamp}
-							</div>
-						</div>
-						<div class="testimonial__author-badge">
-							<div class="testimonial__author">
-								${author}
-							</div>
-							<div class="testimonial__badge">
-								Verifiziert
-							</div>
-						</div>
-						<div class="testimonial__text">
-							“${text}”
-						</div>
-
-					`;
-
-					if(testimonialsWrapper) {
-						testimonialsWrapper.appendChild(reviewSlide);
-
-						const grid = document.querySelector('.bs-product-testimonials__list.grid')
-
-						new Masonry( grid, {
-							itemSelector: '.grid-item',
-							columnWidth: '.grid-item',
-							percentPosition: true,
-							gutter: 10
-							});
-					}
-				}
+				new Masonry( grid, {
+					itemSelector: '.grid-item',
+					columnWidth: '.grid-item',
+					percentPosition: true,
+					gutter: 10
+					});
+			}
+		});
+	
+		const reviewsButton = document.querySelector('[data-js-product-testimonials-button]');
+	
+		reviewsButton.addEventListener('click', () => {
+			const element = document.querySelector('reviewsio-product-reviews-widget');
+			const headerOffset = 300;
+			const elementPosition = element.getBoundingClientRect().top;
+			const offsetPosition = elementPosition + window.scrollY - headerOffset;
+	
+			window.scrollTo({
+				top: offsetPosition,
+				behavior: 'smooth',
 			});
 		});
-
-		mutationObserver.observe(review, {
-			childList: true,
-			subtree: true,
-			attributes: true,
-		});
-	});
-
-	const reviewsButton = document.querySelector('[data-js-product-testimonials-button]');
-
-	reviewsButton.addEventListener('click', () => {
-		const element = document.querySelector('#judgeme_product_reviews');
-		const headerOffset = 300;
-		const elementPosition = element.getBoundingClientRect().top;
-		const offsetPosition = elementPosition + window.scrollY - headerOffset;
-
-		window.scrollTo({
-			top: offsetPosition,
-			behavior: 'smooth',
-		});
-	});
+	}, 2000)
 })();
